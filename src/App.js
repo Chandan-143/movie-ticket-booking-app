519007543c6e611614a6f680b7271780f1d7c185
chandan kumar <chandankumarmech18@gmail.com>
2023-07-18 02:10:17 +0530

    first comit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Layout from './pages/Layout';
+import NoPage from "./pages/NoPage";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import MovieGrid from "./pages/MovieGrid";
+import MovieDetails from "./pages/MovieDetails";
+import MovieList from "./pages/MovieList";
+import MovieTicketPlan from "./pages/MovieTicketPlan";
+import MovieSeatPlan from "./pages/MovieSeatPlan";
+import MovieCheckout from "./pages/MovieCheckout";
+import Popcorn from "./pages/Popcorn";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='/movie-grid' element={<MovieGrid />} />
+            <Route path='/movie-details' element={<MovieDetails />} />
+            <Route path='/movie-list' element={<MovieList />} />
+            <Route path='/movie-ticket-plan' element={<MovieTicketPlan />} />
+            <Route path='/movie-seat-plan' element={<MovieSeatPlan />} />
+            <Route path='/movie-checkout' element={<MovieCheckout />} />
+            <Route path='/popcorn' element={<Popcorn />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+          </Route>
+          <Route path="*" element={<NoPage />} />
+
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
